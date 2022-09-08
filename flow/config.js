a27c3db9486f5aa4cd19310983a614cd7a9b5452
FelipeRibeiroLabs <frlabsmd@gmail.com>
2022-09-08 19:46:48 -0300

    rentContract implementation and usebasedNFT

--- flow/config.js
+++ flow/config.js
@@ -4,5 +4,5 @@ fcl.config({
   "app.detail.title": "Conditional NFTs", // this adds a custom name to our wallet
   "accessNode.api": "https://rest-testnet.onflow.org", // this is for the local emulator
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // this is for the local dev wallet
-  "0xDeployer": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS, // this auto configures `0xDeployer` to be replaced by the address in txs and scripts
+  "0xDeployer": "0xae95963d3be2cd41", // this auto configures `0xDeployer` to be replaced by the address in txs and scripts
 })
